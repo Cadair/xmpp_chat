80ba8714dd86408ee95f1c402b9179993cd9f36e
Stuart Mumford <stuart@cadair.com>
2015-12-10 11:34:27 +0000

    switch to using env vars in docker-compose

---
+++ environment.sh
@@ -0,0 +1,10 @@
+export DOMAIN=localhost
+export XMPP_DOMAIN=localhost
+
+export DB_USER=kaiwa
+export DB_PASS=hello
+
+export LDAP_DOMAIN=localhost
+export LDAP_PASS=hello
+export LDAP_DN="dc=$DOMAIN"
+export LDAP_GROUP=cadair
